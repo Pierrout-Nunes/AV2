d1a8b37ffbd2b37ee6c05ea44aa804fb65d62348
Ana Flávia <pierrout77@gmail.com>
2021-05-25 11:32:31 -0300

    Create TecladoVirtual.java

---
+++ TecladoVirtual.java
@@ -0,0 +1,17 @@
+package tecladovirtual;
+
+import javax.swing.JFrame;
+
+public class TecladoVirtual extends JFrame {
+
+    
+    public static void main(String[] args) {
+        //new Botao();
+        
+        Botao c = new Botao();
+       c.basico(); // para usar o método
+       c.keyboard();
+        
+    }
+    
+}
